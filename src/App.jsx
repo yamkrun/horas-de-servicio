fb28bcc079cd6d62026e40a20dc2360d0a2d1897
Yamila Rengifo <yamirngifo18@gmail.com>
2025-09-09 10:31:36 -0300

    logica del perfil del estudiante para mejor visualización del admin

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,22 @@ import StudentProfile from "./pages/StudentProfile.jsx";
 
 export default function App() {
   return (
-     <AuthProvider>
-    <div className="bg-[#f2f3f7]">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route element={<Layout />}>
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/student" element={<Student />} />
-          <Route path="/student/create-service" element={<CreateService />} />
-          <Route path="/student/evidence/:id" element={<EvidenceViewer />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/updateprofile" element={<UpdateProfile />} />
-             <Route path="/studentprofile" element={<StudentProfile />} />
-        </Route>
-      </Routes>
-    </div>
- </AuthProvider>
-
+    <AuthProvider>
+      <div className="bg-[#f2f3f7]">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route element={<Layout />}>
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/student" element={<Student />} />
+            <Route path="/student/create-service" element={<CreateService />} />
+            <Route path="/student/evidence/:id" element={<EvidenceViewer />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/updateprofile" element={<UpdateProfile />} />
+            <Route path="/studentprofile/:id" element={<StudentProfile />} />
+          </Route>
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 }
